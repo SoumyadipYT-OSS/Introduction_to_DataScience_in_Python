da14fc62f79f0482b14e6d889d4fc1059bdff1f5
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-02-29 14:46:57 +0530

    updated intro3.py

--- 1_Introduction/8_intro3.py
+++ 1_Introduction/8_intro3.py
@@ -7,7 +7,7 @@ Created on Thu Feb 29 14:25:53 2024
 
 
 '''*args: Non-keyword arguments'''
-print("__Example 1__")
+print("__Example 1 (Non-Keyword arguments) looks as tuple()__")
 def f(*args):
     print(type(args))
     print(*args)
@@ -17,7 +17,8 @@ def f(*args):
 f(10,20,30,40,60,70)
 
 
-print("__Example 2__")
+
+print("__Example 2 (Non-Keyword argument) looks as tuple()__")
 def f1(arg1, *args):
     print('Normal Argument: ', arg1)
     print(args)
@@ -25,3 +26,11 @@ print("--test case 1--")
 f1(10,20,30,40,50,60,70)
 print("--test case 2--")
 f1((10,20),30,40,50,60,70)
+
+
+
+print("__Example 3 (Keyword arguments) looks as dictionary__")
+'''**kwargs: Keyword arguments'''
+def f2(**kwargs):
+    print(kwargs)
+f2(First_name='Soumyadip', Last_name='Majumder')
