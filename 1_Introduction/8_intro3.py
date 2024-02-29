bc725f2ab7095d77921ef3c651e812d5ae016a98
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-02-29 15:17:59 +0530

    updated randdomModule file

--- 1_Introduction/8_intro3.py
+++ 1_Introduction/8_intro3.py
@@ -5,7 +5,6 @@ Created on Thu Feb 29 14:25:53 2024
 @author: HP
 """
 
-
 '''*args: Non-keyword arguments'''
 print("__Example 1 (Non-Keyword arguments) looks as tuple()__")
 def f(*args):
@@ -34,3 +33,32 @@ print("__Example 3 (Keyword arguments) looks as dictionary__")
 def f2(**kwargs):
     print(kwargs)
 f2(First_name='Soumyadip', Last_name='Majumder')
+
+
+
+print("__Example 4 (Multiple Keyword and non-keyword arguments)__")
+def f3(arg, *args, **kwargs):
+    print(arg, args, kwargs)
+f3(10,20,30,Course='Data Science',ProgrammingLanguage='Python')
+
+
+
+print("__Example 5 (zip() function)__")
+lst1=[10,20,30]
+lst2=['ITER','SOA','BBSR']
+print(type(zip(lst1, lst2)))
+print(zip(lst1, lst2))
+for i in zip(lst1,lst2):
+    print(i)
+
+
+
+print("__Example 6 (Unzip function)__")
+pairs=[('a',1),('b',2),('c',3)]
+letters,numbers=zip(*pairs)
+print(letters)
+print(numbers)
+
+
+
+print("__Example 7__")
---
+++ 1_Introduction/9_randomModule.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 29 15:16:15 2024
+
+@author: HP
+"""
+
+import random
+random.seed(10)
+print(random.random())
+print(random.random())
