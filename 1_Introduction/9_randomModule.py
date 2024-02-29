53fff442885e32cafe3fb0751442c28d79d8e399
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-02-29 15:20:40 +0530

    updated files

--- 1_Introduction/9_randomModule.py
+++ 1_Introduction/9_randomModule.py
@@ -6,6 +6,7 @@ Created on Thu Feb 29 15:16:15 2024
 """
 
 import random
-random.seed(10)
+random.seed(2)   # seed() values help to generate same number in all computer
 print(random.random())
+random.seed(10)
 print(random.random())
