ec665664f08bdaeb15760c31337e630551d61499
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-07 15:53:50 +0530

    updated to scattered plot

--- 2_Matplotlib/5_ScatteredPlot.py
+++ 2_Matplotlib/5_ScatteredPlot.py
@@ -11,3 +11,9 @@ from matplotlib import pyplot as plt
 users = [70, 65, 72, 63, 71, 64, 60, 64, 67]
 minutes = [175, 170, 205, 120, 220, 130, 105, 145, 190]
 website = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+plt.scatter(users, minutes)
+for label, friend_count, minute_count in zip(website, users, minutes):
+    plt.annotate(label,
+                 xy=(friend_count, minute_count),
+                 xytext=(5, -5),
+                 textcoords='offset points')
