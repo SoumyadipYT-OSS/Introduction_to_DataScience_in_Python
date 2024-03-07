2555aee29b658ffee5feaae40b156a68f4c2ca41
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-07 14:42:53 +0530

    updated

--- 2_Matplotlib/1_LineChart.py
+++ 2_Matplotlib/1_LineChart.py
@@ -10,7 +10,9 @@ from matplotlib import pyplot as plt
 years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
 gdp = [300.2, 543.3, 1074.9, 2862.5, 5979.6, 10289.7, 14958.3]
 # plt.plot(years, gdp, color = "Blue")
+
 plt.plot(years, gdp, color='red', marker='P', linestyle='--')
 plt.title('Year and GDP')
 plt.xlabel('Years')
 plt.ylabel('GDP')
+plt.show()
