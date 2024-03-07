f46525e9672bf440aabe23078b87f197f24f447d
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-07 15:41:38 +0530

    Updated to scattered plot

---
+++ 2_Matplotlib/5_ScatteredPlot.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  7 15:39:08 2024
+
+@author: HP
+"""
+
+
+from matplotlib import pyplot as plt
+
+users = [70, 65, 72, 63, 71, 64, 60, 64, 67]
+minutes = [175, 170, 205, 120, 220, 130, 105, 145, 190]
+website = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
