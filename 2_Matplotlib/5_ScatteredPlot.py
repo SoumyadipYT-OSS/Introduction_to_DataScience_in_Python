c6f350a9c83a31353c91c5b6383f626b1b0a2a50
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-07 15:54:29 +0530

    updated

--- 2_Matplotlib/5_ScatteredPlot.py
+++ 2_Matplotlib/5_ScatteredPlot.py
@@ -12,6 +12,7 @@ users = [70, 65, 72, 63, 71, 64, 60, 64, 67]
 minutes = [175, 170, 205, 120, 220, 130, 105, 145, 190]
 website = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
 plt.scatter(users, minutes)
+
 for label, friend_count, minute_count in zip(website, users, minutes):
     plt.annotate(label,
                  xy=(friend_count, minute_count),
